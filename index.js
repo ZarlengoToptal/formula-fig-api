98d372215ef404779695ca55d61737c3e4a97c38
Chris Zarlengo <chris.zarlengo@toptal.com>
2022-11-23 10:43:19 -0800

    cors

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ console.log({
 const express = require("express");
 const app = express();
 var cors = require("cors");
-app.use(cors());
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions)); // Use this after the variable declaration
 // const db = require("cyclic-dynamodb");
 const { validJWTNeeded } = require("./middleware/authentication");
 
